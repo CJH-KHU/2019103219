5793570e678ef1a05a53c10c4bacaad9a4e86082
LEEJAEBEEN <45088482+woqls22@users.noreply.github.com>
2020-04-17 11:06:52 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 using namespace std;
 
+
+
 int main(){
-	cout << "구구단" << endl
-	
+	cout << "구구단" << endl;
+	cout<<"Modified"<<endl;	
 	for(int i = 1; i < 9; i++){
 		cout << i << "단 : " << end;
 		for(int j = 1; j < 10; j++){
