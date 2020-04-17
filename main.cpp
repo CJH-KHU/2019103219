8bc87c09697d52a961b8157808233f69b421f51f
kyubumJang <jkb2221@khu.ac.kr>
2020-04-17 11:12:09 +0900

    add hello message

--- main.cpp
+++ main.cpp
@@ -6,11 +6,10 @@ int main(){
 	
 	cout << "출력할 구구단 단수를 입력하시오." << endl;
 	cin >> n;
-	
+	cout <<"hello" << endl;
 	cout << n << " 단 : " << endl;
 	for(int i = 1; i < 9; i++)
 		cout << n << " × " << i << " = " << n*i << endl;
-		
 	
 	return 0;
 }	
