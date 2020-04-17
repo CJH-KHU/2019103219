8faa940b192e2aa7336d44034b6f573bd52e25f5
joohyun7u <joohyun7u@khu.ac.kr>
2020-04-17 10:41:07 +0900

    add main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 
 int main(){
-	cout << "test" << endl;
+	int n;
+	
+	cout << "출력할 구구단 단수를 입력하시오." << endl;
+	cin >> n;
+	
+	cout << n << " 단 : " << endl;
+	for(int i = 1; i < 9; i++)
+		cout << n << " × " << i << " = " << n*i << endl;
+		
 	
 	return 0;
 }	
